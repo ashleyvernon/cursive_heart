47d92390b87c27510fc8c45ef2f548d1e5992ba2
Ashley Vernon <avernon526@gmail.com>
2016-06-04 15:20:57 -0700

    ashleys blog

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ from peewee import *
 from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
 from playhouse.sqlite_ext import *
 
-ADMIN_PASSWORD = 'secret'
+ADMIN_PASSWORD = os.environ.get('password')
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
 DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
 DEBUG = False
